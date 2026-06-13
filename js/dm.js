3eae8f198e3de99d09bc049695ed7ead25bca92a
magicgina <magicgina@qq.com>
2026-06-13 12:35:05 +0800

    钟鼓钹磬全键无冲

--- js/dm.js
+++ js/dm.js
@@ -1,21 +1,6 @@
 
 
     window.onload=function() {
-    /*音乐播放一个另一个自动暂停*/
-    var audios = document.getElementsByTagName("audio");
-    // 暂停函数
-    function pauseAll() {
-        var self = this;
-        [].forEach.call(audios, function (i) {
-            // 将audios中其他的audio全部暂停
-            i !== self && i.pause();
-        })
-    }
-    // 给play事件绑定暂停函数
-    [].forEach.call(audios, function (i) {
-        i.addEventListener("play", pauseAll.bind(i));
-    })
-
 
     /*音效按钮和音效组件绑定*/
         document.getElementById('huala').onclick = function(){
